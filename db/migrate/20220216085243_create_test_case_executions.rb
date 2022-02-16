e238a418a88198888f8a9f78127d7bd7c6b62f14
Kentaro Hayashi <hayashi@clear-code.com>
2022-02-16 17:58:21 +0900

    Specify boolean default

--- db/migrate/20220216085243_create_test_case_executions.rb
+++ db/migrate/20220216085243_create_test_case_executions.rb
@@ -1,7 +1,7 @@
 class CreateTestCaseExecutions < ActiveRecord::Migration[5.2]
   def change
     create_table :test_case_executions do |t|
-      t.boolean :result
+      t.boolean :result, null: false, default: false
       t.text :comment
       t.datetime :execution_date
     end
