455423f6e1e12f3d585915f698b425fa9c34d005
Kentaro Hayashi <hayashi@clear-code.com>
2022-02-16 15:03:46 +0900

    test: test initialized TestPlan

--- test/unit/test_plans_test.rb
+++ test/unit/test_plans_test.rb
@@ -2,8 +2,15 @@ require File.expand_path('../../test_helper', __FILE__)
 
 class TestPlansTest < ActiveSupport::TestCase
 
-  # Replace this with your real tests.
-  def test_truth
-    assert true
+  fixtures :projects, :users, :members, :roles
+
+  def test_initialize
+    test_plan = TestPlans.new
+
+    assert_nil test_plan.id
+    assert_nil test_plan.name
+    assert_nil test_plan.begin_date
+    assert_nil test_plan.end_date
+    assert_nil test_plan.estimated_bug
   end
 end
