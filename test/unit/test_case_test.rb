bb5f715f16231a5d3dca27f227f2227b67ccfaaa
Kentaro Hayashi <hayashi@clear-code.com>
2022-02-16 17:27:08 +0900

    Add test case for test case model

--- test/unit/test_case_test.rb
+++ test/unit/test_case_test.rb
@@ -2,8 +2,30 @@ require File.expand_path('../../test_helper', __FILE__)
 
 class TestCaseTest < ActiveSupport::TestCase
 
-  # Replace this with your real tests.
-  def test_truth
-    assert true
+  fixtures :projects, :users, :members, :roles, :issue_statuses
+
+  def test_initialize
+    test_case = TestCase.new
+
+    assert_nil test_case.id
+    assert_nil test_case.user_id
+    assert_nil test_case.issue_status_id
+    assert_nil test_case.project_id
+    assert_nil test_case.name
+    assert_nil test_case.scenario
+    assert_nil test_case.expected
+    assert_nil test_case.environment
+  end
+
+  def test_create
+    test_case = TestCase.new(:id => 1,
+                             :name => "dummy",
+                             :scenario => "test scenario",
+                             :expected => "expected situation",
+                             :environment => "Debian GNU/Linux",
+                             :project => Project.find(1),
+                             :user => User.find(1),
+                             :issue_status => IssueStatus.find(1))
+    assert_save test_case
   end
 end
