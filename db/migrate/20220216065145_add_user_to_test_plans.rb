81ec312806912935a1f5b180dd2811eccd4b55ee
Kentaro Hayashi <hayashi@clear-code.com>
2022-02-16 16:02:41 +0900

    Fix class name

--- db/migrate/20220216065145_add_user_to_test_plans.rb
+++ db/migrate/20220216065145_add_user_to_test_plans.rb
@@ -1,4 +1,4 @@
-class AddUserToTestplans < ActiveRecord::Migration[5.2]
+class AddUserToTestPlans < ActiveRecord::Migration[5.2]
   def change
     add_reference :test_plans, :user, foreign_key: { to_table: :users }
   end
