e2e1f1cf1d278f7aa31e8c9c00f2fa978e1b00ca
Kentaro Hayashi <hayashi@clear-code.com>
2022-02-16 18:41:02 +0900

    Add relations

--- app/models/test_case.rb
+++ app/models/test_case.rb
@@ -2,4 +2,5 @@ class TestCase < ActiveRecord::Base
   belongs_to :user
   belongs_to :project
   belongs_to :issue_status
+  has_many :test_case_executions
 end
--- app/models/test_plan.rb
+++ app/models/test_plan.rb
@@ -1,4 +1,6 @@
 class TestPlan < ActiveRecord::Base
   belongs_to :user
   belongs_to :issue_status
+  has_many :test_case_executions
+  has_many :test_cases
 end
