4b462953b3c3ac3b0b12e8e0925ba821e2e2f05e
Kentaro Hayashi <hayashi@clear-code.com>
2022-02-16 12:10:20 +0900

    Add TestPlans tables
    $ bundle exec ruby bin/rails generate redmine_plugin_model
    redmine_plugin_testcase_management TestPlans name:string
    begin_date:datetime end_date:datetime estimated_bug:integer

---
+++ db/migrate/20220216030922_create_test_plans.rb
@@ -0,0 +1,10 @@
+class CreateTestPlans < ActiveRecord::Migration[5.2]
+  def change
+    create_table :test_plans do |t|
+      t.string :name
+      t.datetime :begin_date
+      t.datetime :end_date
+      t.integer :estimated_bug
+    end
+  end
+end
