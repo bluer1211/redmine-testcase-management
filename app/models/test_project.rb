b106d1b662898cc948aadd1f6b4588721b7f79d1
Kentaro Hayashi <hayashi@clear-code.com>
2022-02-21 16:15:47 +0900

    test: check test_project

--- app/models/test_project.rb
+++ app/models/test_project.rb
@@ -1,4 +1,6 @@
 class TestProject < ActiveRecord::Base
   belongs_to :project
   has_many :test_plans, dependent: :destroy
+
+  validates_associated :test_plans
 end
--- test/unit/test_project_test.rb
+++ test/unit/test_project_test.rb
@@ -2,8 +2,57 @@ require File.expand_path('../../test_helper', __FILE__)
 
 class TestProjectTest < ActiveSupport::TestCase
 
-  # Replace this with your real tests.
-  def test_truth
-    assert true
+  fixtures :projects, :users, :members, :roles, :issue_statuses
+  fixtures :test_projects, :test_plans
+
+  def test_initialize
+    test_project = TestProject.new
+    assert_nil test_project.project_id
   end
+
+  def test_create
+    test_project = TestProject.create(:id => 100)
+    assert_save test_project
+  end
+
+  def test_fixture
+    test_project = TestProject.find(1)
+    assert_equal 1, test_project.id
+  end
+
+  def test_not_unique
+    test_project = TestProject.new(:id => 1)
+    assert_raises ActiveRecord::RecordNotUnique do
+      test_project.save
+    end
+  end
+
+  # Test Relations
+
+  def test_empty_association
+    test_project = TestProject.new
+    assert_nil test_project.project
+  end
+
+  def test_association
+    test_project = TestProject.find(2)
+    assert_equal 1, test_project.project.id
+  end
+
+  def test_no_test_plan
+    test_project = TestProject.find(1)
+    assert_equal 0, test_project.test_plans.size
+  end
+
+  def test_one_test_plan
+    test_project = TestProject.find(2)
+    assert_equal ["Test Plan (No test case)"], test_project.test_plans.pluck(:name)
+  end
+
+  def test_many_test_plan
+    test_project = TestProject.find(3)
+    assert_equal ["Test Plan (1 test case)",
+                  "Test Plan (2 test cases)"], test_project.test_plans.pluck(:name)
+  end
+
 end
