320c9f81399b9d88fbfe925f8d394355f821b41a
Kentaro Hayashi <hayashi@clear-code.com>
2022-02-21 14:43:29 +0900

    Fix migration error

--- db/migrate/20220221052904_create_test_projects.rb
+++ db/migrate/20220221052904_create_test_projects.rb
@@ -3,6 +3,5 @@ class CreateTestProjects < ActiveRecord::Migration[5.2]
     create_table :test_projects do |t|
       t.belongs_to :project, foreign_key: true
     end
-    add_index :test_projects, :project_id
   end
 end
