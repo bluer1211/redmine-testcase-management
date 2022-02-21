6cb190e88801bde9d987a96578a350e635f8fbba
Kentaro Hayashi <hayashi@clear-code.com>
2022-02-21 13:56:17 +0900

    Add missing belongs_to

--- app/models/test_case_execution.rb
+++ app/models/test_case_execution.rb
@@ -3,6 +3,7 @@ class TestCaseExecution < ActiveRecord::Base
   belongs_to :project
   belongs_to :issue
   belongs_to :test_plan
+  belongs_to :test_case
 
   validates :result, presence: true
   validates :comment, presence: true
