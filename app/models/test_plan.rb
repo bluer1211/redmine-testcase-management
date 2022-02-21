1eba008137528535e50de1707698ca2bda8045b1
Kentaro Hayashi <hayashi@clear-code.com>
2022-02-21 14:43:38 +0900

    Add belongs_to

--- app/models/test_plan.rb
+++ app/models/test_plan.rb
@@ -1,6 +1,7 @@
 class TestPlan < ActiveRecord::Base
   belongs_to :user
   belongs_to :issue_status
+  belongs_to :test_project
   has_many :test_case_executions
   has_many :test_cases, dependent: :destroy
 
