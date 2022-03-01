a3cee8421f74bc02d3ea77ea778da090c0b6a99f
Kentaro Hayashi <hayashi@clear-code.com>
2022-03-01 17:49:37 +0900

    test: test creating test plan

--- test/functional/test_plans_controller_test.rb
+++ test/functional/test_plans_controller_test.rb
@@ -1,7 +1,7 @@
 require File.expand_path('../../test_helper', __FILE__)
 
 class TestPlansControllerTest < ActionController::TestCase
-  fixtures :projects
+  fixtures :projects, :users, :issue_statuses
   fixtures :test_projects
 
   def test_index
@@ -9,5 +9,21 @@ class TestPlansControllerTest < ActionController::TestCase
 
     assert_response :success
     #assert_template 'index' # needs rails-controller-testing
+  end
+
+  def test_create_test_plan
+    assert_difference("TestPlan.count") do
+      project_id = test_projects(:test_projects_002).project_id
+      post :create, params: { project_id: project_id, test_plan: { name: "test", user: 2, issue_status: 1 } }
+    end
+    assert_redirected_to project_test_plan_path(:id => TestPlan.last.id)
+  end
+
+  def test_create_missing_test_plan_name
+    assert_no_difference("TestPlan.count") do
+      project_id = test_projects(:test_projects_002).project_id
+      post :create, params: { project_id: project_id, test_plan: { user: 2, issue_status: 1 } }
+    end
+    assert_response :unprocessable_entity
   end
 end
