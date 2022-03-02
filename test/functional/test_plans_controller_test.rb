e8a2969c5eb86f619ebcb3eabcb9c96d06fc18ac
Kentaro Hayashi <hayashi@clear-code.com>
2022-03-02 18:30:36 +0900

    test: add missing fixtures

--- test/functional/test_plans_controller_test.rb
+++ test/functional/test_plans_controller_test.rb
@@ -2,7 +2,7 @@ require File.expand_path('../../test_helper', __FILE__)
 
 class TestPlansControllerTest < ActionController::TestCase
   fixtures :projects, :users, :issue_statuses
-  fixtures :test_projects
+  fixtures :test_projects, :test_plans
 
   def test_index
     get :index, params: { :project_id => test_projects(:test_projects_001).id }
