d9be9942ea5e468513445fabede29f4d83d0a9f0
Kentaro Hayashi <hayashi@clear-code.com>
2022-03-02 13:52:14 +0900

    Add TestCase#create action

--- app/controllers/test_cases_controller.rb
+++ app/controllers/test_cases_controller.rb
@@ -11,7 +11,38 @@ class TestCasesController < ApplicationController
     prepare_user_candidates
   end
 
+  def create
+    prepare_issue_status_candidates
+    prepare_user_candidates
+    @test_case = TestCase.new(:name => test_case_params[:name],
+                              :scheduled_date => test_case_params[:scheduled_date],
+                              :user => User.find(test_case_params[:user]),
+                              :environment => test_case_params[:environment],
+                              :scenario => test_case_params[:scenario],
+                              :expected => test_case_params[:expected],
+                              :issue_status => IssueStatus.find(test_case_params[:issue_status]))
+    if @test_case.valid?
+      @test_case.save
+      flash[:notice] = l(:notice_successful_create)
+      redirect_to project_test_case_path(:id => @test_case.id)
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
   private
+
+  def test_case_params
+    params.require(:test_case).permit(:project_id,
+                                      :test_project_id,
+                                      :name,
+                                      :user,
+                                      :scheduled_date,
+                                      :environment,
+                                      :scenario,
+                                      :expected,
+                                      :issue_status)
+  end
 
   def find_test_project(id_or_name)
     begin
