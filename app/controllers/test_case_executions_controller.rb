d0d5e688f44da9d6a4b7451942214685b058240a
Kentaro Hayashi <hayashi@clear-code.com>
2022-03-03 11:19:08 +0900

    Add TestCaseExecution#new action

--- app/controllers/test_case_executions_controller.rb
+++ app/controllers/test_case_executions_controller.rb
@@ -11,5 +11,33 @@ class TestCaseExecutionsController < ApplicationController
     # FIXME: limit project
     @test_case_executions = TestCaseExecution.all
   end
+
+  def new
+    find_test_project(params.permit(:project_id)[:project_id])
+    @test_case_execution = TestCaseExecution.new
+    # FIXME:
+    # @test_plan = @test_case_execution.test_plan
+    # @test_case = @test_case_execution.test_case
+    @test_plan = TestPlan.find(permit_param(:test_plan_id))
+    @test_case = TestCase.find(permit_param(:test_case_id))
+  end
+
+  private
+
+  def permit_param(symbol)
+    params.permit(symbol)[symbol]
+  end
+
+  def test_case_execution_params
+    params.require(:test_case_execution).permit(:project_id,
+                                                :test_project_id,
+                                                :test_plan_id,
+                                                :test_case_id,
+                                                :name,
+                                                :user,
+                                                :execution_date,
+                                                :comment,
+                                                :result,
+                                                :issue)
   end
 end
