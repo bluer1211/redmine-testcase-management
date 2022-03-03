1e3b4941c279e6411a2c1f3a7192b01d87be227c
Kentaro Hayashi <hayashi@clear-code.com>
2022-03-03 14:05:40 +0900

    Add helper method to confirm TestCaseExecution#destroy

--- app/helpers/test_case_executions_helper.rb
+++ app/helpers/test_case_executions_helper.rb
@@ -1,2 +1,7 @@
 module TestCaseExecutionsHelper
+  def test_case_destroy_confirmation_message(test_case_execution)
+    message = "#{test_case_execution.test_case.name}\n"
+    message << l(:text_test_case_execution_destroy_confirmation)
+    message
+  end
 end
