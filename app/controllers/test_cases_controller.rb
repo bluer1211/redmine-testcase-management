b8e73aae46f35b8822cc41ab724013c4feb8b3bb
YUKI "Piro" Hiroshi <piro.outsider.reflex@gmail.com>
2022-03-04 18:46:05 +0900

    Remove duplicated method

--- app/controllers/test_cases_controller.rb
+++ app/controllers/test_cases_controller.rb
@@ -103,15 +103,4 @@ class TestCasesController < ApplicationController
                                       :issue_status)
   end
 
-  def find_test_project(id_or_name)
-    begin
-      id = Integer(id_or_name)
-      @project = Project.find(id)
-      @test_project = TestProject.where(:project_id => @project.id).first
-    rescue ArgumentError
-      @project = project = Project.where(:name => id_or_name).first
-      @test_project = TestProject.where(:project_id => @project.id).first
-    end
-  end
-
 end
