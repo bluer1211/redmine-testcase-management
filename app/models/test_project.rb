1a50524444aeda212c09bffa995844b16e03a167
Kentaro Hayashi <hayashi@clear-code.com>
2022-03-08 11:10:38 +0900

    Add missing dependent:

--- app/models/test_project.rb
+++ app/models/test_project.rb
@@ -1,8 +1,8 @@
 class TestProject < ActiveRecord::Base
   belongs_to :project
   has_many :test_plans, dependent: :destroy
-  has_many :test_case_executions
-  has_many :test_cases
+  has_many :test_case_executions, dependent: :destroy
+  has_many :test_cases, dependent: :destroy
 
   validates_associated :test_plans
 end
