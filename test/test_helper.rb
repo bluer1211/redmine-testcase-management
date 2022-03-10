7dbfd7fb63a4faf4ab3258b218839e9c8838f5b6
Kentaro Hayashi <hayashi@clear-code.com>
2022-03-10 11:20:47 +0900

    test: add helper to check links

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -7,3 +7,19 @@ def assert_flash_error(message)
     assert_equal message, div.text
   end
 end
+
+def assert_contextual_link(label, path)
+  assert_select "div#content div.contextual a:first-child" do |a|
+    assert_equal path, a.first.attributes["href"].text
+    assert_equal label, a.text
+  end
+end
+
+def assert_back_to_lists_link(path)
+  assert_select "div#content a" do |a|
+    assert_equal path, a.first.attributes["href"].text
+    assert_equal I18n.t(:label_back_to_lists), a.text
+  end
+end
+
+
