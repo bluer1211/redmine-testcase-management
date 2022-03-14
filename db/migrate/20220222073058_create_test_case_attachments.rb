a26ed1e2510281c5af5686fae61ef981d034d3d0
Kentaro Hayashi <hayashi@clear-code.com>
2022-03-14 15:40:43 +0900

    Remove unused migrations

--- db/migrate/20220222073058_create_test_case_attachments.rb
+++
@@ -1,6 +0,0 @@
-class CreateTestCaseAttachments < ActiveRecord::Migration[5.2]
-  def change
-    create_table :test_case_attachments do |t|
-    end
-  end
-end
--- db/migrate/20220224064904_create_test_case_execution_attachments.rb
+++
@@ -1,6 +0,0 @@
-class CreateTestCaseExecutionAttachments < ActiveRecord::Migration[5.2]
-  def change
-    create_table :test_case_execution_attachments do |t|
-    end
-  end
-end
