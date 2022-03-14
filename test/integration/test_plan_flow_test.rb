52e5bbf01dbb98f86d79f3c4d39d54a203680a8a
Kentaro Hayashi <hayashi@clear-code.com>
2022-03-14 18:58:56 +0900

    extract common code

--- test/integration/test_plan_flow_test.rb
+++ test/integration/test_plan_flow_test.rb
@@ -11,16 +11,23 @@ class TestPlanFlowTest < ActionDispatch::IntegrationTest
 
     test_plan = new_record(TestPlan) do
       assert_difference("TestPlan.count") do
-        post url, params: {
-               project_id: projects(:projects_001).identifier,
-               test_plan: {
-                 name: "dummy",
-                 user: 1,
-                 issue_status: 1
-               }
-             }
+        create_test_plan
       end
     end
     assert_redirected_to :controller => 'test_plans', :action => 'show', :id => test_plan.id
+  end
+  private
+
+  def create_test_plan(params={})
+    post_params = {
+      project_id: projects(:projects_001).identifier,
+      test_plan: {
+        name: "dummy",
+        user: 1,
+        issue_status: 1
+      }
+    }
+    post_params.merge!(params)
+    post "/projects/#{projects(:projects_001).identifier}/test_plans", params: post_params
   end
 end
