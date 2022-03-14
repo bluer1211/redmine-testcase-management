8258e484caf50624dfd6ad90fda70c0a5e1f3a41
Kentaro Hayashi <hayashi@clear-code.com>
2022-03-14 15:13:50 +0900

    test: add test plan integration test case

---
+++ test/integration/test_plan_flow_test.rb
@@ -0,0 +1,33 @@
+require 'test_helper'
+
+class TestPlanFlowTest < ActionDispatch::IntegrationTest
+  fixtures :projects, :users, :issues, :issue_statuses
+
+  test "test_plans#index creates test_project automatically" do
+    assert_difference("TestProject.count") do
+      get "/projects/#{projects(:projects_001).identifier}/test_plans"
+    end
+    assert_response :success
+  end
+
+  test "add new test plan" do
+    url = "/projects/#{projects(:projects_001).identifier}/test_plans"
+
+    get "#{url}/new"
+    assert_response :success
+
+    test_plan = new_record(TestPlan) do
+      assert_difference("TestPlan.count") do
+        post url, params: {
+               project_id: projects(:projects_001).identifier,
+               test_plan: {
+                 name: "dummy",
+                 user: 1,
+                 issue_status: 1
+               }
+             }
+      end
+    end
+    assert_redirected_to :controller => 'test_plans', :action => 'show', :id => test_plan.id
+  end
+end
