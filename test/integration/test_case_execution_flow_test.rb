e97020383861d6ac0d0b78d0735a7c8f4ca197fd
Kentaro Hayashi <hayashi@clear-code.com>
2022-03-15 13:26:56 +0900

    test: add test case execution flow test case

---
+++ test/integration/test_case_execution_flow_test.rb
@@ -0,0 +1,84 @@
+require 'test_helper'
+
+class TestCaseExecutionFlowTest < ActionDispatch::IntegrationTest
+  fixtures :projects, :users, :issues, :issue_statuses
+  fixtures :test_plans, :test_cases, :test_case_executions
+
+  def setup
+    @project = projects(:projects_003)
+    @test_plan = test_plans(:test_plans_003)
+    @test_case = test_cases(:test_cases_002)
+  end
+
+  test "add new test case execution" do
+    url = "/projects/#{@project.identifier}/test_plans/#{@test_plan.id}/test_cases/#{@test_case.id}/test_case_executions"
+
+    get "#{url}/new"
+    assert_response :success
+
+    test_case_execution = new_record(TestCaseExecution) do
+      assert_difference("TestCaseExecution.count") do
+        create_test_case_execution
+      end
+    end
+    assert_redirected_to :controller => 'test_case_executions', :action => 'show', :id => test_case_execution.id
+  end
+
+  test "edit test case execution" do
+    test_case_execution = test_case_executions(:test_case_executions_001)
+    url = "/projects/#{@project.identifier}/test_plans/#{@test_plan.id}/test_cases/#{@test_case.id}/test_case_executions/#{test_case_execution.id}"
+
+    get url
+    assert_response :success
+
+    get "#{url}/edit"
+    assert_response :success
+  end
+
+  test "update test case" do
+    test_case_execution = test_case_executions(:test_case_executions_001)
+    url = "/projects/#{@project.identifier}/test_plans/#{@test_plan.id}/test_cases/#{@test_case.id}/test_case_executions/#{test_case_execution.id}"
+
+    get "#{url}/edit"
+    assert_response :success
+
+    put url, params: {
+          project_id: @project_id,
+          test_plan_id: @test_plan.id,
+          test_case_id: @test_case.id,
+          test_case_execution: {
+            result: true,
+            user: 1,
+            comment: "dummy"
+          }
+        }
+    assert_redirected_to :controller => 'test_case_executions', :action => 'show', :id => test_case_execution.id
+  end
+
+  test "delete test plan" do
+    test_case_execution = test_case_executions(:test_case_executions_001)
+    url = "/projects/#{@project.identifier}/test_plans/#{@test_plan.id}/test_cases/#{@test_case.id}/test_case_executions/#{test_case_execution.id}"
+    get url
+    assert_response :success
+
+    delete url
+    assert_redirected_to :controller => 'test_case_executions', :action => 'index'
+  end
+
+  private
+
+  def create_test_case_execution(params={})
+    post_params = {
+      project_id: @project.identifier,
+      test_plan_id: @test_plan.id,
+      test_case_id: @test_case.id,
+      test_case_execution: {
+        user: 1,
+        result: true,
+        comment: "dummy"
+      }
+    }
+    post_params.merge!(params)
+    post "/projects/#{@project.identifier}/test_plans/#{@test_plan.id}/test_cases/#{@test_case.id}/test_case_executions", params: post_params
+  end
+end
