337e19b429cb53d1d138c42113986ce5a214d289
Kentaro Hayashi <hayashi@clear-code.com>
2022-03-31 15:19:54 +0900

    Avoid exception to save custom query for test case executions
    TODO: redirect to correct path

--- lib/tasks/queries_helper_patch.rb
+++ lib/tasks/queries_helper_patch.rb
@@ -3,6 +3,11 @@ module TestCaseManagement
     def redirect_to_test_case_query(options)
       redirect_to project_test_cases_path
     end
+
+    def redirect_to_test_case_execution_query(options)
+      # FIXME: redirect to test case executions list
+      redirect_to project_test_cases_path
+    end
   end
 end
 
