59031a585c2e626c5dc7235913e3f5a3a9d1bbc6
YUKI "Piro" Hiroshi <piro.outsider.reflex@gmail.com>
2022-04-06 17:12:07 +0900

    Implement test case import

--- app/models/test_case.rb
+++ app/models/test_case.rb
@@ -1,4 +1,5 @@
 class TestCase < ActiveRecord::Base
+  include Redmine::SafeAttributes
   include TestCaseManagement::InheritIssuePermissions
 
   belongs_to :user
@@ -16,6 +17,63 @@ class TestCase < ActiveRecord::Base
   validates :project, presence: true
 
   validates_associated :test_case_executions
+
+  safe_attributes(
+    "project_id'",
+    "user_id",
+    "name",
+    "environment",
+    "scenario",
+    "expected",
+    :if => lambda {|test_case, user| test_case.new_record? || test_case.attributes_editable?(user)})
+
+  def safe_attribute_names(user=nil)
+    names = super
+    names << "project_id" if new_record?
+    names
+  end
+
+  def safe_attributes=(attrs, user=User.current)
+    if attrs.respond_to?(:to_unsafe_hash)
+      attrs = attrs.to_unsafe_hash
+    end
+
+    @attributes_set_by = user
+    return unless attrs.is_a?(Hash)
+
+    attrs = attrs.deep_dup
+
+    if (given_project = attrs.delete("project_id")) && safe_attribute?("project_id")
+      if given_project.is_a?(String) && !/^\d*$/.match?(given_project)
+        given_project_id = Project.find_by_identifier(given_project).try(:id)
+      else
+        given_project_id = given_project.to_i
+      end
+      if allowed_target_projects(user).where(:id => given_project_id).exists?
+        self.project_id = given_project_id
+      end
+    end
+
+    assign_attributes attrs
+  end
+
+  def assign_attributes(new_attributes, *args)
+    return if new_attributes.nil?
+
+    attrs = new_attributes.dup
+    attrs.stringify_keys!
+
+    %w(project project_id).each do |attr|
+      if attrs.has_key?(attr)
+        send "#{attr}=", attrs.delete(attr)
+      end
+    end
+    super attrs, *args
+  end
+
+  def attributes=(new_attributes)
+    assign_attributes new_attributes
+  end
 
   scope :visible, (lambda do |*args|
     joins(:project).
@@ -63,5 +121,18 @@ SQL
   # This should be removed after we completely migrate to test_plan-independent design.
   def test_plan
     test_plans.first
+  end
+
+  def ownable_users
+    return [] if project.nil?
+
+    users = project.assignable_users.to_a
+    users.uniq.sort
+  end
+
+  def allowed_target_projects(user=User.current, scope=nil)
+    issue = Issue.new
+    issue.project = project
+    issue.allowed_target_projects(user, scope)
   end
 end
--- app/models/test_case_import.rb
+++ app/models/test_case_import.rb
@@ -1,10 +1,10 @@
 class TestCaseImport < Import
   AUTO_MAPPABLE_FIELDS = {
-    'name' => 'field_name',
-    'user' => 'field_user',
-    'environment' => 'field_environment',
-    'scenario' => 'field_scenario',
-    'expected' => 'field_expected',
+    "name" => "field_name",
+    "user" => "field_user",
+    "environment" => "field_environment",
+    "scenario" => "field_scenario",
+    "expected" => "field_expected",
   }
 
   def self.menu_item
@@ -16,8 +16,17 @@ class TestCaseImport < Import
   end
 
   def saved_objects
-    object_ids = saved_test_cases.pluck(:obj_id)
+    object_ids = saved_items.pluck(:obj_id)
     TestCase.where(:id => object_ids).order(:id)
+  end
+
+  def allowed_target_projects
+    Project.allowed_to(user, :import_test_cases)
+  end
+
+  def project
+    project_id = mapping["project_id"].to_i
+    allowed_target_projects.find_by_id(project_id) || allowed_target_projects.first
   end
 
   def mappable_custom_fields
@@ -29,7 +38,23 @@ class TestCaseImport < Import
   def build_object(row, item)
     test_case = TestCase.new
     test_case.user = user
-    # TBD
+
+    attributes = {
+      "project_id" => mapping["project_id"],
+      "name" => row_value(row, "name"),
+      "environment" => row_value(row, "environment"),
+      "scenario" => row_value(row, "scenario"),
+      "expected" => row_value(row, "expected"),
+    }
+
+    if user_name = row_value(row, "user")
+      if found_user = Principal.detect_by_keyword(test_case.ownable_users, user_name)
+        attributes["user_id"] = found_user.id
+      end
+    end
+
+    test_case.send :safe_attributes=, attributes, user
+
     test_case
   end
 end
