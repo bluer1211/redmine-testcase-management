ea0b6ab98919e2d0f348fdc0c0a112eb0da35df0
Kentaro Hayashi <hayashi@clear-code.com>
2022-04-13 14:35:56 +0900

    test system: check redirect of test case execution

--- test/system/test_case_executions_test.rb
+++ test/system/test_case_executions_test.rb
@@ -31,12 +31,15 @@ class TestCaseExecutionsTest < ApplicationSystemTestCase
   end
 
   test "visiting the index" do
-    visit "/projects/#{@project.identifier}/test_plans/#{@test_plan.id}/test_cases/#{@test_case.id}/test_case_executions"
+    path = "/projects/#{@project.identifier}/test_plans/#{@test_plan.id}/test_cases/#{@test_case.id}/test_case_executions"
+    visit path
     assert_selector "h2", text: I18n.t(:label_test_case_executions)
+    assert_equal path, current_path
   end
 
   test "add new test case execution" do
-    visit "/projects/#{@project.identifier}/test_plans/#{@test_plan.id}/test_cases/#{@test_case.id}/test_case_executions"
+    path = "/projects/#{@project.identifier}/test_plans/#{@test_plan.id}/test_cases/#{@test_case.id}/test_case_executions"
+    visit path
 
     click_on I18n.t(:label_test_case_execution_new)
 
@@ -47,10 +50,13 @@ class TestCaseExecutionsTest < ApplicationSystemTestCase
     fill_in 'test_case_execution[issue_id]', with: "1"
 
     click_button I18n.t(:button_create)
+    path = "/projects/#{@project.identifier}/test_plans/#{@test_plan.id}"
+    assert_equal path, current_path
   end
 
   test "show test case execution" do
-    visit "/projects/#{@project.identifier}/test_plans/#{@test_plan.id}/test_cases/#{@test_case.id}/test_case_executions/#{@test_case_execution.id}"
+    path = "/projects/#{@project.identifier}/test_plans/#{@test_plan.id}/test_cases/#{@test_case.id}/test_case_executions/#{@test_case_execution.id}"
+    visit path
 
     assert_selector "h2", text: "#{I18n.t(:label_test_case_executions)} \##{@test_case_execution.id}"
     assert_selector "h3", text: @test_case_execution.id
@@ -59,10 +65,12 @@ class TestCaseExecutionsTest < ApplicationSystemTestCase
     assert_selector "#user", text: @test_case_execution.user.name
     assert_selector "#execution_date", text: yyyymmdd_date(@test_case_execution.execution_date)
     assert_selector "#comment", text: @test_case_execution.comment
+    assert_equal path, current_path
   end
 
   test "update test case execution" do
-    visit "/projects/#{@project.identifier}/test_plans/#{@test_plan.id}/test_cases/#{@test_case.id}/test_case_executions/#{@test_case_execution.id}/edit"
+    path = "/projects/#{@project.identifier}/test_plans/#{@test_plan.id}/test_cases/#{@test_case.id}/test_case_executions/#{@test_case_execution.id}/edit"
+    visit path
 
     select I18n.t(:label_succeed), from: 'test_case_execution[result]'
     select users(:users_001).name, from: 'test_case_execution[user]'
@@ -71,24 +79,31 @@ class TestCaseExecutionsTest < ApplicationSystemTestCase
     fill_in 'test_case_execution[issue_id]', with: "1"
 
     click_button I18n.t(:button_update)
+    path = "/projects/#{@project.identifier}/test_plans/#{@test_plan.id}/test_cases/#{@test_case.id}"
+    assert_equal path, current_path
   end
 
-  test "delete test case" do
-    visit "/projects/#{@project.identifier}/test_plans/#{@test_plan.id}/test_cases/#{@test_case.id}/test_case_executions/#{@test_case_execution.id}"
+  test "delete test case execution" do
+    path = "/projects/#{@project.identifier}/test_plans/#{@test_plan.id}/test_cases/#{@test_case.id}/test_case_executions/#{@test_case_execution.id}"
+    visit path
 
-    sleep 3
     click_on I18n.t(:button_delete)
     accept_confirm I18n.t(:text_test_case_execution_destroy_confirmation)
+    path = "/projects/#{@project.identifier}/test_plans/#{@test_plan.id}/test_cases/#{@test_case.id}/test_case_executions"
+    sleep 0.1 # wait until deleted
+    assert_equal path, current_path
   end
 
   test "autocomplete issue" do
-    visit "/projects/#{@project.identifier}/test_plans/#{@test_plan.id}/test_cases/#{@test_case.id}/test_case_executions/#{@test_case_execution.id}/edit"
+    path = "/projects/#{@project.identifier}/test_plans/#{@test_plan.id}/test_cases/#{@test_case.id}/test_case_executions/#{@test_case_execution.id}/edit"
+    visit path
 
     fill_in 'test_case_execution[issue_id]', with: "subproject"
     assert_selector "ul#ui-id-1 li", count: 1, text: "Bug ##{issues(:issues_013).id}: #{issues(:issues_013).subject}"
     assert_selector "ul#ui-id-1 li", count: 1, text: "Bug ##{issues(:issues_005).id}: #{issues(:issues_005).subject}"
     page.execute_script "$('ul.ui-autocomplete li:first-child').trigger('mouseenter').click()"
     assert_equal issues(:issues_013).id.to_s, page.evaluate_script("$('#issue_id').val()")
+    assert_equal path, current_path
   end
 
   private
