196dfd346dbdd83929a86cce448854a0ca051883
Kentaro Hayashi <hayashi@clear-code.com>
2022-05-25 14:23:21 +0900

    test system: ensure to wait more

--- test/system/test_cases_test.rb
+++ test/system/test_cases_test.rb
@@ -260,12 +260,14 @@ class TestCasesTest < ApplicationSystemTestCase
     page.document.synchronize do
       page.has_css?("td.scenario")
     end
+    sleep 1 # FIXME: ensure to wait
     assert_selector "td.scenario" do |node|
       assert_equal "<p>1\n</p><p>2\n</p><p>3\n</p><p>4\n</p><p>5\n</p><p>6\n</p><p>7\n</p><p>8\n</p><p>9\n</p><p>10\n11</p>", node[:innerHTML]
     end
     page.document.synchronize do
       page.has_css?("td.expected")
     end
+    sleep 1 # FIXME: ensure to wait
     assert_selector "td.expected" do |node|
       assert_equal "<p>a\n</p><p>b\n</p><p>c\n</p><p>d\n</p><p>e\n</p><p>f\n</p><p>g\n</p><p>h\n</p><p>i\n</p><p>j\nk</p>", node[:innerHTML]
     end
